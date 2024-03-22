4c606a71e92a963a75279865aa092f0d249a1de9
sakilfe <samia.f@escolar.ifrn.edu.br>
2024-03-22 12:02:49 -0300

    pdadc

--- loja/loja/admin.py
+++ loja/loja/admin.py
@@ -3,3 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import * #imporata nossos models
 admin.site.register(Fabricante) #adiciona a interface do adm
+admin.site.register(Categoria)
+admin.site.register(Produto)
